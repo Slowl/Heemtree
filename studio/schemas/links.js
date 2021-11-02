c14977d0c2aaf870e3dd1d15aac23da5af0a32cd
hedi <hedi.kochad@bruce.work>
2021-11-02 23:27:02 +0100

    fix: fixed design issues and added ability to set link orders in studio

--- studio/schemas/links.js
+++ studio/schemas/links.js
@@ -4,6 +4,16 @@ export default {
   type: 'document',
   fields: [
     {
+      name: 'order',
+      title: 'Order',
+      type: 'number',
+      options: {
+        list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        layout: 'radio',
+        direction: 'horizontal'
+      }
+    },    
+    {
       name: 'title',
       title: 'Title',
       type: 'string',
@@ -19,15 +29,26 @@ export default {
       type: 'boolean',
     },
   ],
+  orderings: [
+    {
+      title: 'Order',
+      name: 'order',
+      by: [
+        {field: 'order', direction: 'asc'}
+      ]
+    },
+  ],
 
   preview: {
     select: {
       title: 'title',
+      subtitle: 'order',
     },
     prepare(selection) {
-      const {author} = selection
+      const { title, subtitle } = selection
       return Object.assign({}, selection, {
-        subtitle: author && `by ${author}`,
+        title: `${subtitle} - ${title}`,
+        subtitle: '',
       })
     },
   },
