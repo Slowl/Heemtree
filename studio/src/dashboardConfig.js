92caae433a3664b26631d4ca44d5e99e75fc2872
HediK <20215160+Slowl@users.noreply.github.com>
2021-11-02 23:57:24 +0100

    feat: added netlify dashboard for manual build from studio (#10)
    Co-authored-by: hedi <hedi.kochad@bruce.work>

---
+++ studio/src/dashboardConfig.js
@@ -0,0 +1,19 @@
+export default {
+	widgets: [
+		{
+		name: 'netlify',
+		options: {
+		  title: 'My Netlify deploys',
+		  sites: [
+			{
+			  title: 'Website',
+			  apiId: '74eeb7cf-450e-4a74-8fa5-d4f8cc52a497',
+			  buildHookId: '6181c0a300542051db751eb3',
+			  name: 'heemtree',
+			  url: 'https://heemtree.netlify.app/',
+			}
+		  ]
+		}
+	  }
+	]
+  }
